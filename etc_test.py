3dc5e1cf0a763cad90f84c8fed95c3846e16f0aa
Shin <shinjoohyung25@gmail.com>
2017-04-29 22:37:53 +0900

    update etc_test.py
    all contents updated

--- etc_test.py
+++ etc_test.py
@@ -2,15 +2,26 @@ from blockchain import blockexplorer
 
 block = blockexplorer.get_block("00000000000000000035d467f3e31a04bd9814bc0aec04b7e5877530b41b4450")
 
-val = 0
 
-for i in range (1,len(block.transactions)):
-	#for j in range(len(block.transactions[i].inputs)):
-	#	val += block.transactions[i].inputs[j].value
+#to check whether n_tx includes unconfirmed transactions
+def tx_test():
+    print(block.n_tx)
+    print(len(block.transactions))
+    
+    count = 0
+    for i in range(len(block.transactions)):
+        if (block.transactions[i].block_height != -1): #-1 means unconfirmed
+            count += 1
+    print(count)
 
-	for k in range(len(block.transactions[i].outputs)):
-		val += block.transactions[i].outputs[k].value
+#to discern relationship between total_value, output_values sum, input_values sum
+def values_test():
+    val = 0
+    for i in range (1,len(block.transactions)):
+        #for j in range(len(block.transactions[i].inputs)):
+            val += block.transactions[i].inputs[j].value
+        for k in range(len(block.transactions[i].outputs)):
+            val += block.transactions[i].outputs[k].value
 
-	
+	print(val)
 
-print(val)

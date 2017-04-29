6a738d54aa5fdda52749970fd420c997cc73f0a1
Shin <shinjoohyung25@gmail.com>
2017-04-29 23:29:01 +0900

    in_out_valPrint
    in_out_valPrint 구현

--- blocktx_data.py
+++ blocktx_data.py
@@ -9,6 +9,7 @@
 """
 #import given API
 from blockchain import blockexplorer
+import re
 
 #To make floating point as depicted in the chart
 #Originally it returns int value 281XXX for example
@@ -47,3 +48,35 @@ def avg_tx_size(block_hash):
         tot_size += block.transactions[i].size
     return (tot_size/block.n_tx)
 
+#problem 2
+#Here function input is hash_order which is block_hash (input/output) connected with space
+#e.g [hash_value] input or [hash_value] output
+
+def in_out_valPrint(hash_order):
+    args = hash_order.split()
+    pattern = re.compile("[A-Za-z0-9]+")
+    block_hash = pattern.search(args[0]).group()
+    order = args[1]
+    
+    block = blockexplorer.get_block(block_hash)
+    for transaction in range(1,block.n_tx):
+        n_input = len(block.transactions[transaction].inputs)
+        n_output = len(block.transactions[transaction].outputs)
+        if(order == "input"):
+            for i in range(n_input):
+                input_val_list = [block.transactions[transaction].inputs[i].n, 
+                                  block.transactions[transaction].inputs[i].value,
+                                  block.transactions[transaction].inputs[i].tx_index,
+                                  block.transactions[transaction].inputs[i].script ]
+                print(input_val_list)
+        elif(order == "output"):
+            for j in range(n_output):
+                output_val_list = [block.transactions[transaction].outputs[j].n,
+                                    block.transactions[transaction].outputs[j].value,
+                                    block.transactions[transaction].outputs[j].tx_index,
+                                    block.transactions[transaction].outputs[j].script ]
+                print(output_val_list)
+        else:
+            print("Given order is not definded")
+            
+    return 0

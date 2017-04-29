2cb46bdacd6dfaf643dd06ccd176c9a8d22e9558
Shin <shinjoohyung25@gmail.com>
2017-04-29 23:06:03 +0900

    update
    value debugging

--- testfnt.py
+++ testfnt.py
@@ -15,7 +15,7 @@ def test_tot_tx():
 def test_avg_tx_val():
     checkval1 = e.tot_tx_val("000000000000000000b37c165a79af2a4c2663634d233c0bf09d099e54713bb6")/2757/btcDigit
     checkval2 = e.tot_tx_val("000000000000000001e948e8dae3e14d7edea8514bab7c3dfdb298f0e24f5958")/1423/btcDigit
-    checkval3 = e.tot_tx_val("000000000000000000b37c165a79af2a4c2663634d233c0bf09d099e54713bb6")/1996/btcDigit
+    checkval3 = e.tot_tx_val("000000000000000001d97799bdce61334e0726560f2372bfbf636bf4068f0049")/1996/btcDigit
     assert(abs(b.avg_tx_val("000000000000000000b37c165a79af2a4c2663634d233c0bf09d099e54713bb6") - checkval1) < 0.0000001)
     assert(abs(b.avg_tx_val("000000000000000001e948e8dae3e14d7edea8514bab7c3dfdb298f0e24f5958") - checkval2) < 0.0000001)
     assert(abs(b.avg_tx_val("000000000000000001d97799bdce61334e0726560f2372bfbf636bf4068f0049") - checkval3) < 0.0000001)
@@ -35,7 +35,7 @@ def test_avg_tx_fee():
 def test_avg_tx_size():
     checkval1 = e.tot_tx_size("000000000000000000b37c165a79af2a4c2663634d233c0bf09d099e54713bb6")/2757
     checkval2 = e.tot_tx_size("000000000000000001e948e8dae3e14d7edea8514bab7c3dfdb298f0e24f5958")/1423
-    checkval3 = e.tot_tx_size("000000000000000000b37c165a79af2a4c2663634d233c0bf09d099e54713bb6")/1996
+    checkval3 = e.tot_tx_size("000000000000000001d97799bdce61334e0726560f2372bfbf636bf4068f0049")/1996
     assert(abs(b.avg_tx_size("000000000000000000b37c165a79af2a4c2663634d233c0bf09d099e54713bb6") - checkval1) < 0.0000001)
     assert(abs(b.avg_tx_size("000000000000000001e948e8dae3e14d7edea8514bab7c3dfdb298f0e24f5958") - checkval2) < 0.0000001)
     assert(abs(b.avg_tx_size("000000000000000001d97799bdce61334e0726560f2372bfbf636bf4068f0049") - checkval3) < 0.0000001)

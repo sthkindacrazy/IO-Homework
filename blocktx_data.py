1fe089556bcaf7fa114e21660f70b3456c4b33d4
Shin <shinjoohyung25@gmail.com>
2017-04-29 12:39:41 +0900

    create blocktx_data.py
    design functions for problem1

---
+++ blocktx_data.py
@@ -0,0 +1,24 @@
+"""
+    should calculate 
+    1. total number of transactions
+    2. avg value of transactions
+    3. avg fee of transactions
+    4. avg size of transcations
+    
+    function input is block hash value
+"""
+#import given API
+from blockchain import blockexplorer
+
+def tot_tx(block_hash):
+    return 0
+    
+def avg_tx_val(block_hash):
+    return 0
+    
+def avg_tx_fee(block_hash):
+    return 0
+
+def avg_tx_size(block_hash):
+    return 0
+
